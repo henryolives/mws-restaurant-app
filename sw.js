f43524b4ba3555ef1389475bdf71ff46841011ab
Henry Asante <henryasante10@gmail.com>
2018-10-24 02:06:57 -1200

    paths of resouces to cache corrected

--- sw.js
+++ sw.js
@@ -1,24 +1,30 @@
 // create variables for the cache and the resources to cache
 const CACHE_NAME = 'restaurant-review-cache-v1';
 const resourcesToCache = [
-  '/',
-  '/index.html',
-  '/restaurant.html',
-  '/css/styles.css',
-  '/js/dbhelper.js',
-  '/js/main.js',
-  '/js/restaurant_info.js',
-  '/data/restaurants.json',
-  '/img/1.jpg',
-  '/img/2.jpg',
-  '/img/3.jpg',
-  '/img/4.jpg',
-  '/img/5.jpg',
-  '/img/6.jpg',
-  '/img/7.jpg',
-  '/img/8.jpg',
-  '/img/9.jpg',
-  '/img/10.jpg'
+  './',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/dbhelper.js',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+  'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon-2x.png',
+  'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png',
+  'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
+  
 ];
 
 self.addEventListener('install', function(event) {
